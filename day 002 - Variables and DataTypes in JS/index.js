fd81655b5276398504e0d900bc1ada12da498631
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-04-27 10:52:43 +0530

    day 2

---
+++ day 002 - Variables and DataTypes in JS/index.js
@@ -0,0 +1,56 @@
+// var age = 25;
+
+// if( true){
+//     console.log("var age = " , age);
+// }
+
+// function solve(){
+//     var age = 12;
+//     console.log("var age = " , age);
+// }
+// console.log("var age = " , age);
+// solve();
+
+// var x = 10;
+// console.log("var x = " , x);
+// var x = 20;
+// console.log("var x = " , x);
+
+// var -> function and global scope
+// var -> redeclaration and redefination is possible
+
+// let a = 10;
+// console.log("let a = " , a);
+
+// {
+//     let a = 20;
+//     console.log("let a = " , a);
+// }
+// console.log("let a = " , a);
+
+// let a = 10;
+// console.log("let a = " , a);
+// let a = 20;
+// console.log("let a = " , a);
+// a = 20;
+// console.log("let a = " , a);
+
+// let -> block scope , redecaration and redefination is not possible , but manipulation is possible
+
+// const a = 10;
+// console.log("const a = " , a);
+
+// const a = 20;
+// console.log("const a = " , a);
+// a = 30;
+// console.log("const a = " , a);
+
+// const -> block scope , redecaration , redefination and manipulation is not possible
+
+let marks;
+marks = 20.21;
+marks = "Amit";
+marks = false;
+marks = null;
+marks = 123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890;
+console.log('marks = ' , marks , typeof(marks));
